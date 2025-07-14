6ee81bbdac5d0ca37002ad7dc7ced6234cfbd701
hshhorace <hshhorace@users.noreply.github.com>
2025-07-14 12:00:38 +0000

    deploy: 256e314d798ffa3d1a84ce595755669e743b3888

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a926c9b5a38660c8c77e.js"
+    "url": "webpack-runtime-901fb76e8c14849b1380.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "decfa86b2ba3f3e784f4291e9b4c7e84"
+    "revision": "3a94bd9a7fee2fae2e0af3735a30a9d3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
